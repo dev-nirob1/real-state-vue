fe6178077b7f85a2dc12bade5ead75a328ab492f
dev-nirob1 <dev.nirob1@gmail.com>
2025-03-06 15:42:29 +0600

    gallery page

---
+++ src/components/Gallery/GalleryCard.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="gallery-card">
+        <div class="image-container">
+            <img class="image-responsive" src="./../../assets/about-3.jpg" alt="">
+            <div class="gradient-overlay">
+                <button class="btn-search">
+                    <span class="material-symbols-outlined">
+                        search
+                    </span>
+                </button>
+            </div>
+        </div>
+        <div class="card-content">
+            <h3>Football Tournament</h3>
+            <p class="text-light">Scion Team</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.image-container {
+    position: relative;
+    overflow: hidden;
+    height: 300px;
+}
+
+.image-responsive {
+    height: 100%;
+    transition: transform 1s ease;
+}
+
+.gradient-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8));
+    opacity: 0;
+    transition: opacity 1s ease;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: var(--secondary-color);
+}
+
+.card-content {
+    border: 1px solid var(--border-color);
+    border-top: none;
+    padding: 1rem;
+}
+
+.gallery-card:hover .image-responsive {
+    transform: scale(1.1);
+}
+
+.gallery-card:hover .gradient-overlay {
+    opacity: 1;
+}
+
+.btn-search {
+    padding: 8px;
+    border-radius: 50%;
+    border: none;
+    outline: none;
+    color: var(--dark-color);
+    background-color: var(--secondary-color);
+}
+</style>
